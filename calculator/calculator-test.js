8e012dab130a852fc82b761617c97f2baa788e66
Meryl Dominguez <meryldominguez@gmail.com>
2020-08-24 11:09:06 -0500

    added test case scenarios

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,6 +1,16 @@
 
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  let testAmount1 = {
+    amount: 500000,
+    years: 15,
+    rate:0.03593654,
+  }
+  let testAmount2 = {
+    amount: 300000,
+    years: 10,
+    rate:0.033654,
+  }
+  expect(calculateMonthlyPayment(testAmount1).toEqual())
 });
 
 
