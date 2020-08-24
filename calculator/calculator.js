82507bac95e771b875b451170ec4da82b5377b83
Meryl Dominguez <meryldominguez@gmail.com>
2020-08-24 11:08:49 -0500

    Fixed functionality
    added callbacks, fixed Incorrect equation, now it works

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -8,9 +8,6 @@ window.addEventListener('DOMContentLoaded', function() {
     });
   }
 });
-var amountInput = document.getElementById("loan-amount").value;
-var yearsInput = document.getElementById("loan-years").value;
-const rateInput = document.getElementById("loan-rate").value;
 
 function getCurrentUIValues() {
   return {
@@ -24,30 +21,40 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
-  amountInput.value = 100000;
-  yearsInput.value = 30;
-  rateInput.value = .02967;
-  update()
+  var amount = document.getElementById("loan-amount").value = 100000;
+  var years = document.getElementById("loan-years").value = 30;
+  const rate = document.getElementById("loan-rate").value = .02967;
+  let initial = {
+    amount, years, rate
+  }
+  update();
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
-  let i = rate/12;
-  let P = amount;
-  let n = years*12;
-  var monthly = document.getElementById("monthly-payment").innerText = (P
-    * i)/(1-((1+i)**n))
+updateMonthly(
+  calculateMonthlyPayment(
+    getCurrentUIValues()
+  )
+);
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
-function calculateMonthlyPayment(values) {
+function calculateMonthlyPayment(data) {
+  let i = data.rate/12;
+  let P = data.amount;
+  let n = data.years*12;
+  let monthly = ((P * i)/(1 - ((1+i)**(-n))));
+
+  return `$${(Math.floor(monthly*100))/100}`
+ 
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
-
+  document.getElementById("monthly-payment").innerText = monthly
 }
