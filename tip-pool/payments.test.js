24082ec386e89449cbbb7b1b9bad35422e4df8e6
Meryl Dominguez <meryldominguez@gmail.com>
2020-08-25 17:10:30 -0500

    OMG dont be stupid and initialize document selectors before they exist

--- tip-pool/payments.test.js
+++ tip-pool/payments.test.js
@@ -12,30 +12,31 @@ describe ("createcurrentPayment", ()=>{
         expect(createCurPayment()).toBe(undefined);
     })
 
-})
-describe ("appendPaymentTable", ()=>{
+});
 
-    beforeEach(function(){
-        let tblrow = document.querySelector("#paymentTable tbody trow");
-        let tbltds = document.querySelectorAll("#payment Table tbody trow td");
-        let testPayment = createCurPayment();
+describe ("appendPaymentTable", ()=>{
+    
+    // beforeEach(function(){
         billAmtInput.value = "10";
         tipAmtInput.value = "2";
+        let testPayment = createCurPayment();
         appendPaymentTable(testPayment);
-    })
+        const tblrow = document.querySelector("#payment0");
+        const tbltds = document.querySelectorAll("#payment0 td");  
+    // });
     it("should create a new table row with 3 tds", ()=>{
-        expect(tblrow.innerHTML.trim()).toBe(`<tr id="payment0"><td>$10</td><td>$2</td><td>20%</td></tr>`);;
+        expect(tblrow.innerHTML.trim()).toBe(`<td>$10</td><td>$2</td><td>20%</td>`);
         expect(tbltds.length).toBe(3);
 
-    })
+    });
     it("should add input values into the table", ()=>{
-        expect(tbltds.item(0).innerText).toBeEqualTo("$10");
-        expect(tbltds.item(2).innerText).toBeEqualTo("$2");
-        expect(tbltds.item(2).innerText).toBeEqualTo("20%");
-    })
+        expect(tbltds.item(0).textContent).toBe("$10");
+        expect(tbltds.item(1).innerText).toBe("$2");
+        expect(tbltds.item(2).innerText).toBe("20%");
+    });
     // afterEach(function(){
     //     tblrow.innerHTML = ""
-    // })
+    // });
 })
 describe("updateSummary", ()=>{
     beforeEach(function(){
