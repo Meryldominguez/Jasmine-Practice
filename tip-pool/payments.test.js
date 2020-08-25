506d684f66db7a398bcde38285b622f725c0320f
Meryl Dominguez <meryldominguez@gmail.com>
2020-08-25 17:26:28 -0500

    fixed me being a dumbass with variables

--- tip-pool/payments.test.js
+++ tip-pool/payments.test.js
@@ -16,27 +16,30 @@ describe ("createcurrentPayment", ()=>{
 
 describe ("appendPaymentTable", ()=>{
     
-    // beforeEach(function(){
+    beforeEach(function(){
         billAmtInput.value = "10";
         tipAmtInput.value = "2";
         let testPayment = createCurPayment();
         appendPaymentTable(testPayment);
-        const tblrow = document.querySelector("#payment0");
-        const tbltds = document.querySelectorAll("#payment0 td");  
-    // });
+        
+          
+    });
     it("should create a new table row with 3 tds", ()=>{
+        const tblrow = document.querySelector("#payment0");
+        const tbltds = document.querySelectorAll("#payment0 td");
         expect(tblrow.innerHTML.trim()).toBe(`<td>$10</td><td>$2</td><td>20%</td>`);
         expect(tbltds.length).toBe(3);
 
     });
     it("should add input values into the table", ()=>{
+        const tbltds = document.querySelectorAll("#payment0 td");
         expect(tbltds.item(0).textContent).toBe("$10");
         expect(tbltds.item(1).innerText).toBe("$2");
         expect(tbltds.item(2).innerText).toBe("20%");
     });
-    // afterEach(function(){
-    //     tblrow.innerHTML = ""
-    // });
+    afterEach(function(){
+        document.querySelector("#paymentTable tbody").innerHTML = ""
+    });
 })
 describe("updateSummary", ()=>{
     beforeEach(function(){
